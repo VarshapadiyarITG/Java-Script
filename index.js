7897c23272564caf51308dd4449db7383a0305b8
vasrha <varshapadiyar@itgeeks.com>
2025-08-07 18:45:39 +0530

    Java-Script

--- index.js
+++ index.js
@@ -1,47 +1,69 @@
+//-------------------dark mode change ------------------------------------------------------------
 const btn = document.getElementById('mode-toggle');
 btn.addEventListener('click', () => {
     document.body.classList.toggle('dark');
-     btn.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
+    btn.textContent = document.body.classList.contains('dark') ? '☀️' : '🌙';
 });
-
-
+// -------------------print kar vane ke liye ------------------------------------------------------
+function overWrite() {
+    let input = document.getElementById("myInput");
+    let para = document.getElementById("para");
+    para.innerText = input.value
+}
 const input = document.getElementById("myInput");
 const para = document.getElementById("para")
-// -------------------------------UPPER CASE KE LIYE FUNCTION --------------------------------
+// -------------------------------UPPER CASE KE LIYE FUNCTION --------------------------------------
 const uppreCaseBtn = document.getElementById("uppreCaseBtn");
 uppreCaseBtn.addEventListener("click", function () {
     const input = document.getElementById("myInput").value;
-    para.innerText = input.toUpperCase();
-    
+    para.classList.toggle("uppercase");
+    para.classList.remove("capital")
+    para.classList.remove("lowercase")
+    //para.innerText = input.toUpperCase();
+
 });
-// --------------------LOWER CASE KE LIYE FUNCTION ------------------------------------------
+// --------------------LOWER CASE KE LIYE FUNCTION -------------------------------------------------
 const lowercasebtn = document.getElementById("lowercasebtn");
 lowercasebtn.addEventListener("click", function () {
     const input = document.getElementById("myInput").value;
-    para.innerText = input.toLowerCase();
+    para.classList.toggle("lowercase");
+    para.classList.remove("uppercase")
+    para.classList.remove("capital")
+    // para.innerText = input.toLowerCase();
 });
-// ----------------BOLD KE LIYE FUNCTION -----------------------------------------------------
+// ----------------BOLD KE LIYE FUNCTION ------------------------------------------------------------
 const boldbtn = document.getElementById("boldbtn");
 boldbtn.addEventListener("click", function () {
     const input = document.getElementById("myInput").value;
 
-    para.style.fontWeight = "900";
+    para.classList.toggle("fontweight");
+    // para.style.fontWeight = "900";
 });
-// ----------------------ITALIC KE LIYE FUNCTION -----------------------------------------------
+// ----------------------ITALIC KE LIYE FUNCTION ------------------------------------------------------
 const italic = document.getElementById("italic");
 italic.addEventListener("click", function () {
     const input = document.getElementById("myInput").value;
-    para.style.fontStyle = "italic";
+    para.classList.toggle("italic");
+    // para.style.fontStyle = "italic";
 
 });
-// ------------- COLOR CHANGE FUNCTION -------------------------
-// function changeColor() {
-    
-// }
-//---------------FIRST LETTER CAPITAL---------------------------------------------------------------
+// ------------- COLOR CHANGE FUNCTION -----------------------------------------------------------------
+function changeColor() {
+    let color = document.getElementById("colorinput").value;
+    if (para.style.color == "black") {
+        para.style.color = color;
+    }
+    else {
+        para.style.color = "black"
+    }
+}
+//---------------FIRST LETTER CAPITAL--------------------------------------------------------------------
 const firstletter = document.getElementById("firstletter");
 firstletter.addEventListener("click", function () {
     const input = document.getElementById("myInput").value;
-    para.style.textTransform = "capitalize";
+    para.classList.toggle("capital");
+    para.classList.remove("lowercase")
+    para.classList.remove("uppercase")
+    //para.style.textTransform = "capitalize";
 
 });
